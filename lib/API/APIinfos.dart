ed0ef513b0185c55bc9b5d3f12de738aa1d774b7
pugazhmukilan <pugazhmukilanoffical2004@gmail.com>
2024-07-12 12:46:30 +0530

    small changes

--- lib/API/APIinfos.dart
+++ lib/API/APIinfos.dart
@@ -1,3 +1,5 @@
 const String Authusername = '11184427';
 const String Authpassword = '60-dayfreetrial';
+
+//NOTE this is were you add the actual deployment url for the API calls
 const String Baseurl = 'http://lunchapi-001-site1.etempurl.com';
